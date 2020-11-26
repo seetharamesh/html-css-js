fdf57dce4e4ee737dbafe56a6605a70962046e1d
seetharamesh <seetharamesh@hotmail.com>
2020-11-25 19:37:12 -0500

    changes to prime logic to accomodate printing primes upto a given any number

--- Module-1/WK2-Day2-ML-WhileLoop.js
+++ Module-1/WK2-Day2-ML-WhileLoop.js
@@ -2,10 +2,16 @@
 // 1. Using a while loop, print out all prime numbers between 0 - 20
 var i = 0;
 while (i < 20) {
-  if (i == 0 || i == 1) { /* remember 0 and 1 are not prime*/
+   if(i > 10 && (i%5 == 0 || i%7 == 0)){/*This check is done because multiples of 5 and 7 are to be skipped*/
+      i++;
+      continue;
+      }
+  else if (i == 0 || i == 1) { /* remember 0 and 1 are not prime*/
     i++;
     continue;
-  } else if (i == 2 || i == 3) /* remember 2 and 3 are prime*/
+  }
+
+  else if (i == 2 || i == 3) /* remember 2 and 3 are prime*/
     console.log(i + " is a prime number");
   else if (i % 2 != 0) /* checking if it's not even*/ {
     if (i % 3 != 0) /*checking if it's not odd*/
