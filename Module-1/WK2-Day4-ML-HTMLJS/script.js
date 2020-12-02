92a44a54d51e823ee5669455577d5c670f121ea4
seetharamesh <seetharamesh@hotmail.com>
2020-12-01 22:16:35 -0500

    HTML pages using Javascript

---
+++ Module-1/WK2-Day4-ML-HTMLJS/script.js
@@ -0,0 +1,23 @@
+function sum(a,b){
+	var add = Number(a) + Number(b);
+	alert(add);
+}
+
+function subtract(a,b){
+	var diff = Number(a) - Number(b);
+	alert(diff);
+}
+
+function multiply(a,b){
+	var product = Number(a) * Number(b);
+	alert(product);
+}
+
+function divide(a,b){
+	var result = Number(a) / Number(b);
+	alert(result);
+}
+function avg(a,b){
+  var result = (Number(a) + Number(b))/2;
+  alert(result);
+}
