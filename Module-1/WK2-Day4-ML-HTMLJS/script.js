4024fc5782fb17df4b2fadcddd914061a85f76b9
seetharamesh <seetharamesh@hotmail.com>
2020-12-02 22:02:35 -0500

    added readonly textbox and displayed the result in this box

--- Module-1/WK2-Day4-ML-HTMLJS/script.js
+++ Module-1/WK2-Day4-ML-HTMLJS/script.js
@@ -1,23 +1,29 @@
 function sum(a,b){
-	var add = Number(a) + Number(b);
-	alert(add);
+	var c = Number(a) + Number(b);
+	document.getElementById("result").value = c;
+
+//	alert(add);
 }
 
 function subtract(a,b){
-	var diff = Number(a) - Number(b);
-	alert(diff);
+	var c = Number(a) - Number(b);
+	document.getElementById("result").value = c;
+//	alert(diff);
 }
 
 function multiply(a,b){
-	var product = Number(a) * Number(b);
-	alert(product);
+	var c = Number(a) * Number(b);
+	document.getElementById("result").value = c;
+	//alert(product);
 }
 
 function divide(a,b){
-	var result = Number(a) / Number(b);
-	alert(result);
+	var c = Number(a) / Number(b);
+	document.getElementById("result").value = c;
+	//alert(result);
 }
 function avg(a,b){
-  var result = (Number(a) + Number(b))/2;
-  alert(result);
+  var c = (Number(a) + Number(b))/2;
+	document.getElementById("result").value = c;
+//  alert(result);
 }
